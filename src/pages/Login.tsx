1823119e7ef9bf0df4390f696b9b0c992ec1d771
MAGHC <windowsapple95@gmail.com>
2022-08-30 20:41:34 +0900

    [style]:로그인 페이지 마크업

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,66 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { FormContainer } from "../Copmonents";
 
 const Wrapper = styled.div(
   ({ theme }) => `
   width: 100%;
   height: 100vh;
   ${theme.mixin.center()}
+  flex-direction:column;
 `
 );
 
 const Title = styled.h1(
   ({ theme }) => `
   color:${theme.colors.font};
-  font-size: 4rem;
+  font-size: 3.5rem;
   text-align:center;
   padding:2rem;
 `
 );
 
 const ContentContainer = styled.div`
-  width: 40rem;
-  height: 40rem;
+  width: 30rem;
+  height: 20rem;
+  border: 1px solid ${({ theme }) => theme.colors.primaryDark};
+  border-radius: 10%;
+  padding: 1rem;
+  flex-direction: column;
+`;
+
+const IdPwWrap = styled.div``;
+
+const FromInput = styled(FormContainer.Input)`
+  display: block;
+  width: 96%;
+  height: 2.5rem;
+  box-shadow: none;
+
+  &:focus {
+    border: 5px solid ${({ theme }) => theme.colors.secondaryDark};
+  }
+`;
+
+const Inner = styled.div`
+  padding: 4rem;
+`;
+
+const ButtonWrap = styled.div`
+  margin-top: 3rem;
+`;
+const FromButton = styled(FormContainer.Button)`
+  width: 100%;
+  height: 2.5rem;
   background-color: ${({ theme }) => theme.colors.primaryDark};
+  border: none;
+  border-radius: 2px;
+  font-size: 1.3rem;
+  color: ${({ theme }) => theme.colors.primaryLight};
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Login = () => {
@@ -31,8 +70,19 @@ const Login = () => {
   });
   return (
     <Wrapper>
+      <Title>LOGIN</Title>
       <ContentContainer>
-        <Title>Login</Title>
+        <FormContainer>
+          <Inner>
+            <IdPwWrap>
+              <FromInput placeholder="아이디" type="email"></FromInput>
+              <FromInput placeholder="비밀번호" type="password"></FromInput>
+            </IdPwWrap>
+            <ButtonWrap>
+              <FromButton>로그인</FromButton>
+            </ButtonWrap>
+          </Inner>
+        </FormContainer>
       </ContentContainer>
     </Wrapper>
   );
