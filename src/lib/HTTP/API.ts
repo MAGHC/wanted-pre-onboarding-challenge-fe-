48d9b1e8f1bb7a95780de884527c43d87f1f6cec
MAGHC <windowsapple95@gmail.com>
2022-09-01 19:53:01 +0900

    [API]:add 공통 처리

--- src/lib/HTTP/API.ts
+++ src/lib/HTTP/API.ts
@@ -2,9 +2,17 @@ import axios from "axios";
 
 export const BASE_URL = "http://localhost:8080";
 
-// export const AP = {
-//   todos: `${BASE_URL}/todos`,
-//   auth: `${BASE_URL}/users`,
-// };
-
 export const API = axios.create({ baseURL: BASE_URL, timeout: 1000, headers: { "Content-Type": "application/json" } });
+
+API.interceptors.response.use(
+  (res) => {
+    console.log(res);
+    if (res.data.token) {
+      localStorage.setItem("token", res.data.token);
+    }
+    return res;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
