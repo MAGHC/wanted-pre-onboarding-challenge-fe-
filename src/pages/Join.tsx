b7885adda0bd117d758ed4598c4706f3f513ba96
MAGHC <windowsapple95@gmail.com>
2022-09-02 19:13:10 +0900

    [delete]formcontainer 삭제

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import styled from "styled-components";
+import { FormContainer } from "@/Copmonents";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  ${({ theme }) => theme.mixin.center()}
+`;
+
+const FromContainer = styled.``
 
 const Join = () => {
-  return <div></div>;
+  return <Wrapper>테스트</Wrapper>;
 };
 
 export default Join;
