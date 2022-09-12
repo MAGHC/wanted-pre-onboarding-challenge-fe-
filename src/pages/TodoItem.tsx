3abab5f12afb7ccdc18dbe1de54bfb68df68da2b
MAGHC <windowsapple95@gmail.com>
2022-09-12 18:57:48 +0900

    [Style]:간단한 todoItem 스타일

--- src/pages/TodoItem.tsx
+++ src/pages/TodoItem.tsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  ${({ theme }) => theme.mixin.center()};
+  border: 1px solid ${({ theme }) => theme.colors.primaryDark};
+`;
+
+const Item = styled.p`
+  font-size: 2rem;
+  margin: 0 2rem;
+`;
+
+const DelteBtn = styled.button`
+  margin: 0 2rem;
+`;
+
+const EditBtn = styled.button`
+  margin: 0 2rem;
+`;
+
 const TodoItem = () => {
-  return <div>아이템1</div>;
+  return (
+    <Wrapper>
+      <Item>간단한 메모 내용</Item>
+      <EditBtn>수정</EditBtn>
+      <DelteBtn>삭제</DelteBtn>
+    </Wrapper>
+  );
 };
 
 export default TodoItem;
