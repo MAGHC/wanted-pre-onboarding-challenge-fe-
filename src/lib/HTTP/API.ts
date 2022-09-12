ea6da2f130a759debf32721692b6dcf95ea15c96
MAGHC <windowsapple95@gmail.com>
2022-09-12 19:20:54 +0900

    [API]:axios.create() header token 추가

--- src/lib/HTTP/API.ts
+++ src/lib/HTTP/API.ts
@@ -2,7 +2,22 @@ import axios from "axios";
 
 export const BASE_URL = "http://localhost:8080";
 
-export const API = axios.create({ baseURL: BASE_URL, timeout: 1000, headers: { "Content-Type": "application/json" } });
+interface Token {
+  token: string;
+}
+
+const getToken = () => {
+  const data = localStorage.getItem("token");
+  if (data) {
+    const { token } = JSON.parse(data) as Token;
+    return token;
+  }
+  return false;
+};
+
+const token = getToken();
+
+export const API = axios.create({ baseURL: BASE_URL, timeout: 1000, headers: { "Content-Type": "application/json", Authorization: token } });
 
 API.interceptors.response.use(
   (res) => {
